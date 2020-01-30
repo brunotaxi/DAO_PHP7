538467e385d4d780ac992cbec97fceb56c002d12
BrunoTaxi78 <brunotaxi78@gmail.com>
2020-01-30 19:31:05 +0000

    Metodos de Lista

--- index.php
+++ index.php
@@ -4,8 +4,23 @@ require_once("config.php");
 
 $user = new Usuario();
 
-$user->loadById(3);
+//Mostra apenas 1 usuario através do ID
+//$user->loadById(3);
+//echo $user;
+
+
+//Carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de Usuarios através do login
+//$search = Usuario::search("Br");
+//echo json_encode($search);
+
+//Carrega um usuario usando o login e senha
+$user->login("user","12345");
 
 echo $user;
+
 
  ?>
